5daa3ba59a6f8b19a2a3884391c4ffdbd53d359c
MrDavot <36482903+MrDavot@users.noreply.github.com>
2024-03-04 17:08:51 +0500

    Add files via upload

---
+++ src/pages/calc/Calc.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './calc.scss'
+
+
+export const Calc = () =>{
+
+    return(
+        <h1>Calculator</h1>
+    )
+}
---
+++ src/pages/info/Info.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './info.scss'
+
+
+export const Info = () =>{
+
+    return(
+        <h1>info</h1>
+    )
+}
